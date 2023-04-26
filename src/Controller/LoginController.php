d8f84635241e08242ea6ea645cb4f12398b35578
AhmedDaali <ahmeddaali2@gmail.com>
2023-04-26 15:55:59 +0200

    logging-out

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -21,4 +21,8 @@ class LoginController extends AbstractController
             'error' => $error
         ]);
     }
+    #[Route('/logout', name: 'app_logout')]
+    public function logout()
+    {
+    }
 }
